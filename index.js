86cf161c5cd701b20bc3fa88519a465268f82fea
andy <andy.bruere@gmail.com>
2017-09-16 20:47:44 +0200

    Fastboot compatibility

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 /* jshint node: true */
 'use strict';
 
+var MergeTrees = require('broccoli-merge-trees');
+var Funnel     = require('broccoli-funnel');
+var map        = require('broccoli-stew').map;
+
 module.exports = {
   name: 'ember-circle-progress',
 
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    app.import(app.bowerDirectory + '/jquery-circle-progress/dist/circle-progress.js');
-  }
+    app.import('vendor/circle-progress.js');
+  },
+
+  treeForVendor(vendorTree) {
+    let trees = [];
+    let circleProgress = new Funnel('bower_components/jquery-circle-progress/dist', {
+      files: ['circle-progress.js']
+    });
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    circleProgress = map(circleProgress, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+
+    trees.push(circleProgress);
+
+    return new MergeTrees(trees);
+},
 
 };
